3293e7427bb854228418f22d7f831be087ac89b2
FloCodin <137909251+FloCodin@users.noreply.github.com>
2024-08-28 16:59:59 +0200

    feat: remove button className

--- src/app/components/todos/PriorityTodo.tsx
+++ src/app/components/todos/PriorityTodo.tsx
@@ -17,7 +17,6 @@ const PriorityTodo = ({ todo }: { todo: todoProps }) => {
             <input type="hidden" name="inputId" value={todo.id} />
             <Button
                 type="submit"
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
                 text={<GiFlyingFlag />}
             >
             </Button>
